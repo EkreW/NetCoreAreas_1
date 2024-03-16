00f8124d19152795e1de5b4ec3caf63941558eee
Ekrem Güngör <ekrmdsgnr@gmail.com>
2024-03-16 18:13:54 +0300

    Add project files.

---
+++ NetCoreAreas_1/Areas/Admin/Controllers/HomeController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace NetCoreAreas_1.Areas.Admin.Controllers
+{
+    public class HomeController : Controller
+    {
+        //Area'lar Attribute olarak tanımlanmadan calıstırılamaz
+        [Area("Admin")]
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
